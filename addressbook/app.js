4e4293ad399803f41bc4716758a944730e02c669
tomas f <46322567+TomFern@users.noreply.github.com>
2020-02-04 10:01:56 -0300

    add database code, person routes

--- addressbook/app.js
+++ addressbook/app.js
@@ -7,6 +7,9 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+// tutorial
+var personsRouter = require('./routes/persons');
+
 var app = express();
 
 // view engine setup
@@ -21,6 +24,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+// tutorial
+app.use('/persons', personsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
