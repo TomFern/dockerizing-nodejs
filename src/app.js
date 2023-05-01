118f5cb6bb4f92ddf536a9b08341ab3f56200280
tomas f <46322567+TomFern@users.noreply.github.com>
2023-05-01 12:24:26 -0300

    cleanup

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
+// sample express app created with npx express-generator --no-view src
+
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-const indexRouter = require('./routes/index');
 const personsRouter = require('./routes/persons');
 
 const app = express();
@@ -14,7 +15,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
 app.use('/persons', personsRouter);
 
 module.exports = app;
