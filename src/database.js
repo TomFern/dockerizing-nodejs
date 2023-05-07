4373c827da548dd65bca8c842f9e10d10c439617
tomas f <46322567+TomFern@users.noreply.github.com>
2023-05-07 14:15:26 -0300

    db password

--- src/database.js
+++ src/database.js
@@ -3,7 +3,7 @@
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize(process.env.DB_SCHEMA || 'postgres',
                                 process.env.DB_USER || 'postgres',
-                                process.env.DB_PASSWORD || '',
+                                process.env.DB_PASSWORD || process.env.POSTGRES_PASSWORD ||  '',
                                 {
                                     host: process.env.DB_HOST || 'localhost',
                                     port: process.env.DB_PORT || 5432,
